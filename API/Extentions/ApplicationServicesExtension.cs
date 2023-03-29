7d1eb83bfdfe7552eb7a914eea216066643b65e0
shabanov300 <shabanov300@gmail.com>
2023-03-29 13:52:24 +0300

    End of Section 6

--- API/Extentions/ApplicationServicesExtension.cs
+++ API/Extentions/ApplicationServicesExtension.cs
@@ -41,6 +41,16 @@ namespace API.Extentions
                     return new BadRequestObjectResult(errorResponse);
                 };
             });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod()
+                    .WithOrigins("https://localhost:4200");
+                });
+            });
+
             return services;
          }
     }
