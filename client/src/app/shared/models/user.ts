3e4c9c541d762120b4869d6afb4ca259d207037b
shabanov300 <shabanov300@gmail.com>
2023-04-11 17:07:18 +0300

    End of Section 17

---
+++ client/src/app/shared/models/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+    email: string;
+    displayName: string;
+    token: string;
+}
+
+export interface Address {
+    firstName: string;
+    lastName: string;
+    Street: string;
+    City: string;
+    State: string;
+    ZipCode: string;
+}
